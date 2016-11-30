f525134872443d83de14fa66967f336e6f6f285c
aki01 <test2@test.com>
2016-12-01 01:10:08 +0900

    update

--- bench.py
+++ bench.py
@@ -1,122 +1,15 @@
-import subprocess
-import csv
 import sys
-import datetime
-import os
+import bench_timecmd as bt
 
 args = sys.argv
 argc = len(args)
-if (argc != 3):
-    print("Usage: python %s arg1 arg2" % args[0])
+if (argc < 2):
+    print("Usage: python %s run_file [loop_num]" % args[0])
     quit()
-
-dir_name = "data"
-if not os.path.isdir(dir_name):
-    os.mkdir(dir_name)
-
-file_name = args[1]
-run_num = int(args[2])
-
-date = datetime.date.today().strftime("%Y%m%d")
-name = re.sub('\w+/', "", file_name).replace('.rb', '')
-log_name = '%s_%s.txt' % (name,date)
-csv_name = '%s_%s.csv' % (name,date)
-log_path = dir_name + "/" + log_name
-csv_path = dir_name + "/" + csv_name
-
-def cmd_run(cmd):
-    p = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    #print("%s\n%s\n%d\n" % (p.stdout.decode('utf-8'),p.stderr,p.returncode))
-    return p
-
-def run(*args):
-    param = args
-    date = datetime.datetime.today().strftime("%Y%m%d %H:%M:%S")
-    cmd = "ruby %s" % file_name
-    for arg in args:
-        cmd += (" %s" % str(arg))
-    ret = cmd_run(cmd)
-
-    if (ret.returncode == 0):
-        # write txt
-        f = open(log_path,'a')
-        f.write("%s %s\n" % (date,str(args)))
-        f.write("%s\n" % ret.stdout.decode('utf-8'))
-        f.close()
-
-        # write csv
-        results = ret.stdout.decode('utf-8').split("\n")
-        results.remove('')
-        f = open(csv_path, 'a')
-        writer = csv.writer(f, lineterminator='\n')
-        writer.writerow([date,param])
-        f.close()
-        for result in results:
-            # [num:bench[s]]
-            loop_num,run_time = result.split(":")
-            f = open(csv_path, 'a')
-            writer = csv.writer(f, lineterminator='\n')
-            writer.writerow([loop_num,run_time])
-            f.close()
-    else:
-        # write txt
-        f = open(log_path,'a')
-        f.write("%s %s\n" % (date,str(args)))
-        f.write("%s\n" % ret.stderr.decode('utf-8'))
-        f.close()
-
-def run_loop(loop_num,*args):
-    param = args
-    date = datetime.datetime.today().strftime("%Y%m%d %H:%M:%S")
-    cmd = "ruby %s" % file_name
-    for arg in args:
-        cmd += (" %s" % str(arg))
-
-    f = open(csv_path, 'a')
-    writer = csv.writer(f, lineterminator='\n')
-    writer.writerow([date,param])
-    f.close()
-
-    num = 0
-    count = 0
-    while num < loop_num:
-        ret = cmd_run(cmd)
-        if (ret.returncode == 0):
-            # write txt
-            f = open(log_path,'a')
-            f.write("%s %s\n" % (date,str(args)))
-            f.write("%s\n" % ret.stdout.decode('utf-8'))
-            f.close()
-
-            # write csv
-            results = ret.stdout.decode('utf-8').split("\n")
-            results.remove('')
-            for result in results:
-                # [num:bench[s]]
-                run_index,run_time = result.split(":")
-                f = open(csv_path, 'a')
-                writer = csv.writer(f, lineterminator='\n')
-                writer.writerow([num,run_time])
-                f.close()
-            num += 1
-        else:
-            # write txt
-            f = open(log_path,'a')
-            f.write("%s %s\n" % (date,str(args)))
-            f.write("%s\n" % ret.stderr.decode('utf-8'))
-            f.close()
-
-        count += 1
-        if (loop_num*2<count):
-            f = open(log_path,'a')
-            f.write("not run loop_num times\n")
-            f.close()
-            break
-
-    f = open(csv_path, 'a')
-    writer = csv.writer(f, lineterminator='\n')
-    writer.writerow("")
-    f.close()
+elif (argc == 2):
+    bench = bt.Bench(args[1])
+else:
+    bench = bt.Bench(args[1],args[2])
 
 for i in range(1,11):
-    run_loop(run_num,i*100,i*100)
+    bench.run_loop_time(i*100,i*100)
--- bench_timecmd.py
+++ bench_timecmd.py
@@ -5,86 +5,134 @@ import datetime
 import re
 import os
 
-args = sys.argv
-argc = len(args)
-if (argc != 3):
-    print("Usage: python %s arg1 arg2" % args[0])
-    quit()
+class Bench:
 
-dir_name = "data"
-if not os.path.isdir(dir_name):
-    os.mkdir(dir_name)
+    def __init__(self, name, num=1):
+        dir_name = "data"
 
-file_name = args[1]
-run_num = int(args[2])
+        if not os.path.isdir(dir_name):
+            os.mkdir(dir_name)
 
-date = datetime.date.today().strftime("%Y%m%d")
-name = re.sub('\w+/', "", file_name).replace('.rb', '')
-log_name = '%s_%s.txt' % (name,date)
-csv_name = '%s_%s.csv' % (name,date)
-log_path = dir_name + "/" + log_name
-csv_path = dir_name + "/" + csv_name
+        self.file_name = name
+        self.run_num = int(num)
+
+        date = datetime.date.today().strftime("%Y%m%d")
+        name = re.sub('\w+/', "", self.file_name).replace('.rb', '')
+        log_name = '%s_%s.txt' % (name,date)
+        csv_name = '%s_%s.csv' % (name,date)
+        self.log_path = dir_name + "/" + log_name
+        self.csv_path = dir_name + "/" + csv_name
 
 
-def cmd_run(cmd):
-    p = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    #print("%s\n%s\n%d\n" % (p.stdout.decode('utf-8'),p.stderr,p.returncode))
-    return p
+    def cmd_run(self,cmd):
+        p = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        #print("%s\n%s\n%d\n" % (p.stdout.decode('utf-8'),p.stderr,p.returncode))
+        return p
 
-def run_loop_time(loop_num,*args):
-    param = args
-    date = datetime.datetime.today().strftime("%Y%m%d %H:%M:%S")
-    cmd = "/usr/bin/time -p ruby %s" % file_name
-    for arg in args:
-        cmd += (" %s" % str(arg))
+    def run_loop_time(self,*args):
+        param = args
+        date = datetime.datetime.today().strftime("%Y%m%d %H:%M:%S")
+        cmd = "/usr/bin/time -p ruby %s" % self.file_name
+        for arg in args:
+            cmd += (" %s" % str(arg))
 
-    f = open(csv_path, 'a')
-    writer = csv.writer(f, lineterminator='\n')
-    writer.writerow([date,param])
-    f.close()
+        f = open(self.csv_path, 'a')
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerow([date,param])
+        f.close()
 
-    num = 0
-    count = 0
-    while num < loop_num:
-        ret = cmd_run(cmd)
-        if (ret.returncode == 0):
-            # write txt
-            f = open(log_path,'a')
-            f.write("%s %s\n" % (date,str(args)))
-            f.write("%s\n" % ret.stdout.decode('utf-8'))
-            f.write("%s\n" % ret.stderr.decode('utf-8'))
-            f.close()
-
-            # write csv
-            time_out = ret.stderr.decode('utf-8')
-            a = re.search('real ([a-zA-Z0-9_.]+)\n', time_out)
-            #a = re.search('real\t([a-zA-Z0-9_.]+)\n', time_out)
-            if (a != None):
-                time_ms = re.findall('[0-9.]+',a.group(1))
-                #tims_second = float(time_ms[0]) * 60.0 + float(time_ms[1])
-
-                f = open(csv_path, 'a')
-                writer = csv.writer(f, lineterminator='\n')
-                writer.writerow([num,time_ms[0]])
+        num = 0
+        count = 0
+        while num < self.run_num:
+            ret = self.cmd_run(cmd)
+            if (ret.returncode == 0):
+                # write txt
+                f = open(self.log_path,'a')
+                f.write("%s %s\n" % (date,str(args)))
+                f.write("%s\n" % ret.stdout.decode('utf-8'))
+                f.write("%s\n" % ret.stderr.decode('utf-8'))
                 f.close()
 
-            num += 1
-        else:
-            # write txt
-            f = open(log_path,'a')
-            f.write("%s %s\n" % (date,str(args)))
-            f.write("%s\n" % ret.stdout.decode('utf-8'))
-            f.write("%s\n" % ret.stderr.decode('utf-8'))
-            f.close()
+                # write csv
+                time_out = ret.stderr.decode('utf-8')
+                a = re.search('real ([a-zA-Z0-9_.]+)\n', time_out)
+                #a = re.search('real\t([a-zA-Z0-9_.]+)\n', time_out)
+                if (a != None):
+                    time_ms = re.findall('[0-9.]+',a.group(1))
+                    #tims_second = float(time_ms[0]) * 60.0 + float(time_ms[1])
 
-        count += 1
-        if (loop_num*2<count):
-            break
+                    f = open(self.csv_path, 'a')
+                    writer = csv.writer(f, lineterminator='\n')
+                    writer.writerow([num,time_ms[0]])
+                    f.close()
 
-    f = open(csv_path, 'a')
-    writer = csv.writer(f, lineterminator='\n')
-    writer.writerow("")
-    f.close()
+                num += 1
+            else:
+                # write txt
+                f = open(self.log_path,'a')
+                f.write("%s %s\n" % (date,str(args)))
+                f.write("%s\n" % ret.stdout.decode('utf-8'))
+                f.write("%s\n" % ret.stderr.decode('utf-8'))
+                f.close()
 
-for i in range(1,11):
-    run_loop_time(run_num,i*100,i*100)
+            count += 1
+            if (self.run_num*2<count):
+                break
+
+        f = open(self.csv_path, 'a')
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerow("")
+        f.close()
+
+    def run_loop(self,*args):
+        param = args
+        date = datetime.datetime.today().strftime("%Y%m%d %H:%M:%S")
+        cmd = "ruby %s" % self.file_name
+        for arg in args:
+            cmd += (" %s" % str(arg))
+
+        f = open(self.csv_path, 'a')
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerow([date,param])
+        f.close()
+
+        num = 0
+        count = 0
+        while num < self.run_num:
+            ret = self.cmd_run(cmd)
+            if (ret.returncode == 0):
+                # write txt
+                f = open(self.log_path,'a')
+                f.write("%s %s\n" % (date,str(args)))
+                f.write("%s\n" % ret.stdout.decode('utf-8'))
+                f.close()
+
+                # write csv
+                results = ret.stdout.decode('utf-8').split("\n")
+                results.remove('')
+                for result in results:
+                    # [num:bench[s]]
+                    run_index,run_time = result.split(":")
+                    f = open(self.csv_path, 'a')
+                    writer = csv.writer(f, lineterminator='\n')
+                    writer.writerow([num,run_time])
+                    f.close()
+                num += 1
+            else:
+                # write txt
+                f = open(self.log_path,'a')
+                f.write("%s %s\n" % (date,str(args)))
+                f.write("%s\n" % ret.stderr.decode('utf-8'))
+                f.close()
+
+            count += 1
+            if (self.run_num*2<count):
+                f = open(self.log_path,'a')
+                f.write("not run loop_num times\n")
+                f.close()
+                break
+
+        f = open(self.csv_path, 'a')
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerow("")
+        f.close()
